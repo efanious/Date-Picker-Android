4b805a3ef7deec1f6a2ebe42ad4efc0761acdb51
Efana Usua <Efana.usua@wemabank.com>
2020-04-23 16:52:51 +0100

    works well

--- MainActivity.cs
+++ MainActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System;
 
 namespace DatePicker
 {
@@ -18,7 +19,17 @@ namespace DatePicker
             SetContentView(Resource.Layout.activity_main);
 
             dateEditText = FindViewById<EditText>(Resource.Id.dateEditText);
+
+            dateEditText.Click += DateEditText_Click;
         }
-        
+
+        private void DateEditText_Click(object sender, System.EventArgs e)
+        {
+            DatePickerFragment frag = DatePickerFragment.NewInstance(delegate (DateTime time)
+            {
+                dateEditText.Text = time.ToLongDateString();
+            });
+            frag.Show(FragmentManager, DatePickerFragment.TAG);
+        }
     }
 }
